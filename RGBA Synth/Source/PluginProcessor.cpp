815ea5abf39f0511a85c7e4059a09467532e6975
Jacob Canedy <jcanedy19@gmail.com>
2023-01-12 11:19:07 -0500

    Build Fix

--- RGBA Synth/Source/PluginProcessor.cpp
+++ RGBA Synth/Source/PluginProcessor.cpp
@@ -1,10 +1,4 @@
-/*
-  ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
