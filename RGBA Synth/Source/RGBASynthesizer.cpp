7ec39968679cf96c1f93804a61ef3e2ab1a2f62d
Jacob Canedy <jcanedy19@gmail.com>
2023-01-17 16:58:59 -0500

    Sounds RGBA Synth can Make

--- RGBA Synth/Source/RGBASynthesizer.cpp
+++ RGBA Synth/Source/RGBASynthesizer.cpp
@@ -17,5 +17,12 @@ RGBASynthesizer::RGBASynthesizer()
 
 void RGBASynthesizer::noteOn(int midiChannel, int midiNoteNumber, float velocity)
 {
-    
+    //set midiChannel notenumber and velocity of all synth sounds?
+
+    for (auto voice = 0; voice < getNumVoices(); voice++)
+    {
+        auto currentVoice = getVoice(voice);
+        currentVoice->setKeyDown(true);
+        currentVoice->startNote(midiNoteNumber, velocity, )
+    }
 }
